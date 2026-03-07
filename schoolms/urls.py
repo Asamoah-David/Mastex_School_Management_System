bdc7fd3cd6ff829653e0c01cdf0bebe1cdbfab76
Asamoah-David <asamoadavi6917@gmail.com>
2026-03-07 06:27:32 -1200

    Fix homepage 404

--- schoolms/urls.py
+++ schoolms/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from accounts.views import dashboard
 
 urlpatterns = [
+    path("", dashboard, name="home"),
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view()),
