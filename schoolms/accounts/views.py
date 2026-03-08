fea0387f7e5190e7f93e4bbb76fa68f75cd67ed0
Asamoah-David <asamoadavi6917@gmail.com>
2026-03-07 12:38:49 -1200

    Fixing server error 500

--- schoolms/accounts/views.py
+++ schoolms/accounts/views.py
@@ -9,7 +9,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect("dashboard")
+            return redirect("home")
     return render(request, "accounts/login.html")
 
 @login_required
